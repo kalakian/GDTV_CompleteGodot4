94dd6da54fcda8057dea9ec85a637b2835e9b254
Derek Stobbs <dstobbs@googlemail.com>
2023-06-12 15:36:20 +0100

    Player movement
    * Added basic player controls with physics movement

--- speedy_saucer/player.gd
+++ speedy_saucer/player.gd
@@ -1,11 +1,18 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _physics_process(delta):
+	var force = 1000
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
