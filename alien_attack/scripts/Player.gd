f54ee09ecbd7aab23491c32d14d0c830321931e2
Derek Stobbs <dstobbs@googlemail.com>
2023-06-14 14:41:19 +0100

    Alien Attack: Shooting
    * Player can now shoot rockets

--- alien_attack/scripts/Player.gd
+++ alien_attack/scripts/Player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 var speed = 300
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
 func _physics_process(delta):
 	velocity = Vector2.ZERO
 	
@@ -18,4 +24,8 @@ func _physics_process(delta):
 	
 	var screen_size = get_viewport_rect().size
 	global_position = global_position.clamp(Vector2.ZERO, screen_size)
-	
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 80
--- alien_attack/scripts/rocket.gd
+++ alien_attack/scripts/rocket.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed = 400
+@export var speed = 500
 
 func _physics_process(delta):
 	global_position.x += speed * delta
