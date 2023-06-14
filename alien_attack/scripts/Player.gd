1f19d3f7cb031c1bbd804d3629d732adf969cdb5
Derek Stobbs <dstobbs@googlemail.com>
2023-06-14 12:52:41 +0100

    Alien Attack: Player Movement
    * Player has basic movement using arrow keys or WASD

--- alien_attack/scripts/Player.gd
+++ alien_attack/scripts/Player.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
+var speed = 300
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y += speed
+	
+	move_and_slide()
