73adcd5c8ba1101669a7d38598a52e8605f99e22
Derek Stobbs <dstobbs@googlemail.com>
2023-06-14 15:13:33 +0100

    Alien Attack: Rocket Cleanup
    * Rockets no longer change position with the player
    * Rockets are destroyed when leaving the screen

--- alien_attack/scripts/Player.gd
+++ alien_attack/scripts/Player.gd
@@ -4,6 +4,8 @@ var speed = 300
 
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
+@onready var rocket_container = $RocketContainer
+
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -27,5 +29,6 @@ func _physics_process(delta):
 
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
