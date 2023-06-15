db3a3e4d4fd86a05b456e965865c473a9d45ab07
Derek Stobbs <dstobbs@googlemail.com>
2023-06-15 13:46:01 +0100

    Alien Attack: Enemies Spawning
    * Enemies now spawning in every two seconds at a random position

---
+++ alien_attack/scripts/enemy_spawner.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
+@onready var spawn_positions = $SpawnPositions
+@onready var enemy_container = $EnemyContainer
+
+func _on_timer_timeout():
+	spawn_enemy()
+
+func spawn_enemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	
+	var enemy_instance = enemy_scene.instantiate()
+	enemy_container.add_child(enemy_instance)
+	enemy_instance.global_position = random_spawn_position.global_position
