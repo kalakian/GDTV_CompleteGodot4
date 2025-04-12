5a9b51687ca4242262b707898c3f014c4495612c
Kalakian <dstobbs@googlemail.com>
2025-04-12 15:24:28 +0100

    Martian Mike: Deathzone
    - Player's position and veocity are reset if they fall into a deathzone that's just below the ground
    - Camera is limited so it doen't show below the ground now

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+@onready var start_position = $StartPosition
+
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+
+func _on_deathzone_body_entered(body):
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
