8d89ac5f98fdfb4a56584faa6052382a519c73cd
Kalakian <dstobbs@googlemail.com>
2025-04-12 12:36:41 +0100

    Martian Mike: Player Movement
    - The Player can now fall, jump, and move left or right

--- martian_mike/scripts/player.gd
+++ martian_mike/scripts/player.gd
@@ -1,7 +1,21 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var max_fall_speed = 500
+@export var jump_force = 200
+@export var speed = 125
+
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(_delta):
-	if Input.is_action_just_pressed("move_right"):
-		animated_sprite.play("run")
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if velocity.y > max_fall_speed:
+			velocity.y = max_fall_speed
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	move_and_slide()
